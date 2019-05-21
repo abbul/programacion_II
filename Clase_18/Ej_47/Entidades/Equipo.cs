81384f056d7de447e4daf1cef4f3f957a4a5fbcf
abbul <abbulrodriguez@hotmail.com>
2019-05-21 20:19:08 -0300

    ej_47

---
+++ Clase_18/Ej_47/Entidades/Equipo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Equipo
+    {
+        private string nombre;
+        private DateTime fechaDeCreacion;
+
+        protected Equipo(string nombre, DateTime fechaDeCreacion)
+        {
+            Nombre = nombre;
+            FechaDeCreacion = fechaDeCreacion;
+        }
+
+        public string Nombre { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+
+        public string Ficha()
+        {
+            return "Nombre: " + Nombre + "Fecha de Creacion: " + FechaDeCreacion;
+        }
+
+
+    }
+}
---
+++ Clase_18/Ej_47/Entidades/Torneo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Torneo
+    {
+        private List<Equipo> equipos;
+        private string nombre;
+
+        public static  bool operator ==(Equipo p, Torneo e)
+        {
+            foreach (Equipo item in e.equipos )
+            {
+                if ( item.Nombre == p.Nombre && item.FechaDeCreacion == p.FechaDeCreacion)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Equipo p, Torneo e)
+        {
+            return (p == e) ? false : true;
+        }
+
+        public static bool operator +(Equipo p, Torneo e)
+        {
+            if(p != e)
+            {
+                e.equipos.Add(p);
+                return true;
+            }
+
+            return false;
+            
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("TORNEO: " + nombre);
+
+            foreach (Equipo item in equipos)
+            {
+                sb.AppendLine(item.Ficha());
+            }
+
+            return Convert.ToString(sb);
+        }
+
+        private string CalcularPartido(Equipo uno, Equipo dos)
+        {
+            Random r = new Random();
+
+            return uno.Nombre + r.Next() + " - " + r.Next() + dos.Nombre;
+        }
+
+        public string CalcularPartido()
+        {
+
+        }
+    }
+}
